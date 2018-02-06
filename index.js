f9eb3b50b21466fb840349326bf1037bf8d287fe
Josh <josuedor@gmail.com>
2018-02-05 22:54:29 -0500

    Commit inicial documentacion

--- index.js
+++
@@ -1,6 +0,0 @@
-const config = require('./server/config');
-const app = require('./server');
-
-app.listen(config.port, config.hostname, () => {
-  console.log(`Server running at http://${config.hostname}:${config.port}/`);
-});
